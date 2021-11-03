85bde3a1d22a0b5edabec1c333ce2dd8487c4d69
David Link <david.link@tommicopper.com>
2021-11-03 12:20:33 -0400

    can not test front or back of card

--- vcards.py
+++ vcards.py
@@ -28,7 +28,16 @@ class VCards():
         cards.append(card2)
         return cards
 
-    def test(self):
+    def test(self, test_side='front'):
+        if test_side == 'front':
+            question = 'front'
+            answer = 'back'
+            answer_name = 'back_name'
+        else:
+            question = 'back'
+            answer = 'front'
+            answer_name = 'front_name'
+
         n = 0
         correct = 0
         queue = self.cards
@@ -40,35 +49,44 @@ class VCards():
             card = queue[card_num]
             del queue[card_num]
 
+            if test_side == 'front':
+                question = card.front
+                answer = card.back
+                answer_name = card.back_name
+            else:
+                question = card.back
+                answer = card.front
+                answer_name = card.front_name
+
             # print card
-            ind = self.printCard(n, card)
+            ind = self.printCard(n, question)
 
             # get answer
-            ans = input(ind + card.back_name + ": ")
+            guess = input(ind + answer_name + ": ")
 
             # break out if nec.
-            if ans == 'q':
+            if guess == 'q':
                 print('End Session')
                 break
             print()
 
             # check answer
-            if ans == card.back:
+            if guess.lower() == answer.lower():
                 correct += 1
                 print(ind + 'Correct')
             else:
-                print(ind + 'Incorrect. %s' % card.back)
+                print(ind + 'Incorrect. %s' % answer)
 
             # show score
             print("%sScore: %s/%s" % (ind, correct, n))
             print()
             
-    def printCard(self, n, card):
+    def printCard(self, n, question):
         nstr = str(n)
         ind = ' ' * (len(nstr)+2)
-        print('%s.--%s--.' % (ind, '-'*len(card.front)))
-        print('%s  |  %s  |' % (nstr, card.front))
-        print('%s.--%s--.' % (ind, '-'*len(card.front)))
+        print('%s.--%s--.' % (ind, '-'*len(question.encode('utf-8'))))
+        print('%s  |  %s  |' % (nstr, question))
+        print('%s.--%s--.' % (ind, '-'*len(question.encode('utf-8'))))
         return ind
 
 class Card():
@@ -80,5 +98,5 @@ class Card():
         self.back = back
 
 if __name__ == '__main__':
-    VCards().test()
+    VCards().test('back')
     
